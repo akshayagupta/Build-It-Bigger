4d8fbeb356b45407ea656c32ffb5d2f840032aad
Akshaya Gupta <akshaya@zophop.com>
2016-04-28 02:50:43 +0530

    update

--- FinalProject/app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
+++ FinalProject/app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
@@ -1,13 +1,26 @@
 package com.udacity.gradle.builditbigger;
 
 import android.app.Application;
+import android.test.ActivityInstrumentationTestCase2;
 import android.test.ApplicationTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
+public class ApplicationTest extends ApplicationTestCase {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    public void testAsynctask() {
+
+        EndpointsAsyncTask endpointsAsyncTask = new EndpointsAsyncTask() {
+            @Override
+            public void onPostExecute(String joke) {
+                assertNotNull(joke);
+                assertTrue(joke.length() != 0);
+            }
+        };
+        endpointsAsyncTask.execute(getContext());
     }
 }
